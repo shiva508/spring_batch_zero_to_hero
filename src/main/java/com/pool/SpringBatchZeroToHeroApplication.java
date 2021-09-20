4ddbbab3c5dad4be47308f1b66b0c19cd419804d
shiva508 <dasarishiva1@gmail.com>
2021-09-20 20:12:25 +0530

    first commit

---
+++ src/main/java/com/pool/SpringBatchZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchZeroToHeroApplication.class, args);
+	}
+
+}
